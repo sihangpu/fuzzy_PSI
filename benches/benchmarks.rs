4648c587387a33bee7fdbbeb0d96b205d0920a07
Sihang Pu <sihang.pu@gmail.com>
2023-09-29 19:32:34 +0200

    okvs tested

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,47 +1,170 @@
 #![feature(test)]
+#![allow(dead_code)]
 extern crate test;
-
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_TABLE;
-use curve25519_dalek::scalar::Scalar;
-use curve25519_dalek::RistrettoPoint;
-
-use rand::rngs::OsRng;
-use std::collections::HashMap;
 
 extern crate f_psi;
 use f_psi::okvs;
 
 #[cfg(test)]
-#[allow(unused_variables)]
 mod tests {
     use super::*;
+    use rand::Rng;
+    use std::time::Instant;
     use test::Bencher;
 
+    use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+    use curve25519_dalek::constants::RISTRETTO_BASEPOINT_TABLE;
+    use curve25519_dalek::scalar::Scalar;
+    use curve25519_dalek::RistrettoPoint;
+
+    use fxhash::hash64;
+    use rand::rngs::OsRng;
+    use std::collections::HashMap;
+
     #[test]
-    fn okvs_works() {
+    fn hash_test() {
+        //test hash64
+        let h1 = hash64(&(0 ^ 0x1234567890abcdefu64));
+        let h1c = hash64(&(0 ^ 0x1234567890abcdefu64));
+        let seed = hash64(&(0 ^ 0x1234567890abcdefu64));
+        let hash_band: Vec<Scalar> = (0..40).map(|i| Scalar::from((seed >> i) & 0x01)).collect();
+        //print hash_band
+        for i in 0..40 {
+            print!("{:?} ", hash_band[i] == Scalar::ONE);
+        }
+        println!("h1: {:x}", h1);
+        assert_eq!(h1, h1c);
+    }
+    #[test]
+    fn gbf_test() {
         let mut list: HashMap<u64, RistrettoPoint> = HashMap::new();
         let n = 100;
         for j in 0..n {
             list.insert(j, RISTRETTO_BASEPOINT_POINT);
         }
-        let m = (n as f64 * (1.0 + okvs::EPSILON)).ceil() as u64;
-        let encoding = okvs::okvs_encode(&list);
-        let decoding = okvs::okvs_decode(&encoding, 1);
-        assert_eq!(encoding.len(), m as usize);
+        let mut gbf = okvs::GBF::new(n);
+        gbf.encode(&list);
+        let decoding = gbf.decode(12);
         assert_eq!(decoding, RISTRETTO_BASEPOINT_POINT);
     }
 
+    #[test]
+    fn okvs_test() {
+        let mut list: HashMap<u64, Scalar> = HashMap::new();
+        let n = 2000;
+        for j in 0..n {
+            list.insert(j + 1, Scalar::ONE);
+        }
+        let mut okvsmod = okvs::OKVS::new(n);
+
+        let now = Instant::now();
+
+        test::black_box(okvsmod.encode(&list));
+
+        let elapsed = now.elapsed();
+        println!("Elapsed Enc: {:.2?}", elapsed);
+
+        let now2 = Instant::now();
+        let decoding = okvsmod.decode(39);
+        let elapsed2 = now2.elapsed();
+
+        println!("Elapsed Dec: {:.2?}", elapsed2);
+        assert_eq!(decoding, RISTRETTO_BASEPOINT_POINT);
+    }
+    #[bench]
+    fn bench_gbf_encode(b: &mut Bencher) {
+        let mut list: HashMap<u64, RistrettoPoint> = HashMap::new();
+        let n = 2000;
+        for j in 0..n {
+            list.insert(j, RISTRETTO_BASEPOINT_POINT);
+        }
+        let mut gbf = okvs::GBF::new(n);
+        b.iter(|| {
+            test::black_box(gbf.encode(&list));
+        });
+    }
+
+    #[bench]
+    fn bench_okvs_encode(b: &mut Bencher) {
+        let mut list: HashMap<u64, Scalar> = HashMap::new();
+        let n = 1000;
+        for j in 0..n {
+            list.insert(j + 1, Scalar::ONE);
+        }
+        let mut okvsMod = okvs::OKVS::new(n);
+        b.iter(|| {
+            test::black_box(okvsMod.encode(&list));
+        });
+    }
+
+    #[bench]
+    fn bench_gbf_decode(b: &mut Bencher) {
+        let mut list: HashMap<u64, RistrettoPoint> = HashMap::new();
+        let n = 2000;
+        for j in 0..n {
+            list.insert(j, RISTRETTO_BASEPOINT_POINT);
+        }
+        let mut gbf = okvs::GBF::new(n);
+        b.iter(|| {
+            test::black_box(gbf.decode(8));
+        });
+    }
+
+    fn add_n(p1: &RistrettoPoint, p2: &RistrettoPoint, n: usize) -> RistrettoPoint {
+        let mut p: RistrettoPoint = RISTRETTO_BASEPOINT_POINT;
+        for _ in 0..n {
+            test::black_box(p = p1 - p2);
+        }
+        hash64(&9238u64);
+        return p;
+    }
+    fn vec_it(v: &Vec<RistrettoPoint>) {
+        for i in 0..v.len() {
+            test::black_box(v[i]);
+        }
+    }
     #[bench]
     fn bench_pointadd(b: &mut Bencher) {
         let mut rng = OsRng;
         // Generate two random points on the curve.
         let point_ran1 = RistrettoPoint::random(&mut rng);
         let point_ran2 = RistrettoPoint::random(&mut rng);
+        let mut list: HashMap<u64, RistrettoPoint> = HashMap::new();
+
+        let n = 2000;
+        let m = (n as f64 * okvs::FACTOR).floor() as u64;
+        for j in 0..n {
+            list.insert(j, RISTRETTO_BASEPOINT_POINT);
+        }
+        let mut enc: Vec<RistrettoPoint> =
+            (0..m).map(|_| RistrettoPoint::random(&mut OsRng)).collect();
         b.iter(|| {
-            test::black_box(&point_ran1 + &point_ran2);
+            // test::black_box(add_n(&point_ran1, &point_ran2, 2000 * 40));
+            vec_it(&enc);
+            // test::black_box(okvs::gbf_encode(&list, &mut enc));
         });
     }
+
+    fn scalar_fp(s: &Scalar, t: &Scalar, n: usize) {
+        let mut r = Scalar::ZERO;
+        for _ in 0..n {
+            r = s.invert();
+            for _ in 0..40 {
+                r += r * t;
+            }
+        }
+    }
+    #[bench]
+    fn bench_scalar(b: &mut Bencher) {
+        let scalar1 = Scalar::from(5u64);
+        let scalar2 = Scalar::from(7u64);
+        b.iter(|| {
+            // test::black_box(&scalar1.invert());
+            test::black_box(scalar1 == Scalar::ZERO);
+            // test::black_box(scalar_fp(&scalar1, &scalar2, 2000));
+        });
+    }
+
     #[bench]
     fn bench_scalarmult(b: &mut Bencher) {
         let scalar = Scalar::from(5u64);
--- src/okvs.rs
+++ src/okvs.rs
@@ -1,88 +1,232 @@
+const EPSILON: f64 = 0.15;
+const STAT_BITS: u64 = 40;
+pub const FACTOR: f64 = STAT_BITS as f64 * 1.44 as f64;
+// const STAT_MASK: u64 = (1 << STAT_BITS) - 1;
+// const POS_MASK: u64 = 1 << (STAT_BITS - 1);
 
-pub const EPSILON: f64 = 0.15;
-pub const STAT_BITS: u64 = 40;
-const STAT_MASK: u64 = (1 << STAT_BITS) - 1;
-const POS_MASK: u64 = 1 << STAT_BITS;
+const HASH_SEED: u64 = 0x1234567890abcdef;
 
+use curve25519_dalek::constants::RISTRETTO_BASEPOINT_TABLE;
 use curve25519_dalek::traits::Identity;
 use curve25519_dalek::RistrettoPoint;
+use curve25519_dalek::Scalar;
+
 use fxhash::hash64;
 use rand::rngs::OsRng;
+// use rand::Rng;
+use core::usize::MAX;
 use std::collections::HashMap;
 
-pub fn okvs_encode(list: &HashMap<u64, RistrettoPoint>) -> Vec<RistrettoPoint> {
-    // Implementation goes here
-    let n: u64 = list.len() as u64;
-    let m: u64 = (n as f64 * (1.0 + EPSILON)).ceil() as u64;
-    let mut pivots: Vec<u64> = vec![0u64; n as usize];
-    let pos_band_range: u64 = m - STAT_BITS;
-    let mut matrix: Vec<(u64, (u64, RistrettoPoint))> = Vec::with_capacity(n as usize);
-    let mut result: Vec<RistrettoPoint> =
-        (0..m).map(|_| RistrettoPoint::random(&mut OsRng)).collect();
-    let mut hash_pos: u64;
-    let mut hash_band: u64;
+type Point = RistrettoPoint;
+// type Point = i64;
 
-    for (&key, &value) in list.iter() {
-        hash_pos = hash64(&key) % pos_band_range;
-        hash_band = hash64(&hash_pos) & STAT_MASK;
-        matrix.push((hash_pos, (hash_band, value)));
-    }
-    // sort by position
-    matrix.sort_by(|a, b| a.0.cmp(&b.0));
-    for row in 0..n {
-        let (pos, (band, value)) = matrix[row as usize];
-        for i in 0..STAT_BITS {
-            if 0 == (band & (POS_MASK >> i)) {
-                continue;
-            }
-            pivots[row as usize] = i + pos;
-            for j in row + 1..n {
-                if matrix[j as usize].0 > pivots[row as usize] {
-                    continue;
-                }
-                // shift bands to get bits under the pivot
-                let to_compare: u64 =
-                    matrix[j as usize].1 .0 << (pivots[row as usize] - matrix[j as usize].0);
-                if 0 != (to_compare & POS_MASK) {
-                    matrix[j as usize].1 .0 ^= band << (matrix[j as usize].0 - pos);
-                    matrix[j as usize].1 .1 += value;
-                }
-            }
-            break;
-        }
-    }
-    // back substitution
-    for row in (0..n).rev() {
-        let (pos, (band, value)) = matrix[row as usize];
-        result[pos as usize] = value;
-        for i in 0..STAT_BITS {
-            if 0 == (band & (POS_MASK >> i)) {
-                continue;
-            }
-            if i + pos == pivots[row as usize] {
-                continue;
-            }
-            result[pos as usize] = result[pos as usize] - result[(i + pos) as usize];
-        }
-    }
-    return result;
+// fn random_ind(l: usize) -> usize {
+//     let mut rng = OsRng;
+//     let index = rng.gen_range(0..l);
+//     return index;
+// }
+
+pub struct GBF {
+    data: Vec<Point>,
+    m: u64,
+    n: u64,
+    kappa: u64,
 }
 
-pub fn okvs_decode(okvs: &Vec<RistrettoPoint>, key: u64) -> RistrettoPoint {
-    // Implementation goes here
-    let m: u64 = okvs.len() as u64;
-    let pos_band_range: u64 = m - STAT_BITS;
-    let pos: u64;
-    let band: u64;
-    pos = hash64(&key) % pos_band_range;
-    band = hash64(&pos) & STAT_MASK;
-    let mut result = RistrettoPoint::identity();
-
-    for i in 0..STAT_BITS {
-        if 0 == (band & (POS_MASK >> i)) {
-            continue;
-        }
-        result += okvs[(i + pos) as usize];
+impl GBF {
+    pub fn new(num_item: u64) -> Self {
+        let m: u64 = (num_item as f64 * FACTOR).floor() as u64;
+        let kappa: u64 = STAT_BITS;
+        let data: Vec<Point> = (0..m).map(|_| Point::random(&mut OsRng)).collect();
+        return GBF {
+            data,
+            m,
+            n: num_item,
+            kappa,
+        };
     }
-    return result;
+    pub fn num_items(&self) -> u64 {
+        return self.n;
+    }
+    pub fn len(&self) -> u64 {
+        return self.m;
+    }
+    pub fn num_hashes(&self) -> u64 {
+        return self.kappa;
+    }
+    pub fn encode(&mut self, list: &HashMap<u64, Point>) {
+        let mut touched: Vec<bool> = vec![false; self.m as usize];
+        let mut hashkey: [u64; STAT_BITS as usize] = [0u64; STAT_BITS as usize];
+        for (&key, &value) in list.iter() {
+            let seed = hash64(&(key ^ HASH_SEED));
+            for i in 0..STAT_BITS {
+                hashkey[i as usize] = hash64(&(seed ^ i)) % self.m;
+            }
+            let mut temp = value;
+            let mut pos = 0usize;
+            for i in 0..STAT_BITS as usize {
+                let j = hashkey[i] as usize;
+                if false == touched[j] {
+                    pos = j;
+                    touched[j] = true;
+                }
+                temp -= self.data[j];
+            }
+            assert!(pos != 0usize);
+            self.data[pos] += temp;
+        }
+    }
+
+    pub fn decode(&mut self, key: u64) -> Point {
+        let seed: u64 = hash64(&(key ^ HASH_SEED));
+        let mut hashkey = [0u64; STAT_BITS as usize];
+        for i in 0..STAT_BITS {
+            hashkey[i as usize] = hash64(&(seed ^ i)) % self.m;
+        }
+
+        let mut result = Point::identity();
+        for i in 0..STAT_BITS as usize {
+            result += self.data[hashkey[i] as usize];
+        }
+        return result;
+    }
+}
+
+pub struct OKVS {
+    data: Vec<Point>,
+    _data: Vec<Scalar>,
+    _matrix: Vec<(usize, (Vec<Scalar>, Scalar))>,
+    m: u64,
+    n: u64,
+    kappa: u64,
+    epsilon: f64,
+}
+impl OKVS {
+    pub fn new(num_item: u64) -> Self {
+        let m: u64 = (num_item as f64 * (1.0 + EPSILON)).ceil() as u64;
+        let _data: Vec<Scalar> = (0..m).map(|_| Scalar::random(&mut OsRng)).collect();
+        return OKVS {
+            data: Vec::with_capacity(m as usize),
+            _data,
+            _matrix: Vec::with_capacity(num_item as usize),
+            m,
+            n: num_item,
+            kappa: STAT_BITS,
+            epsilon: EPSILON,
+        };
+    }
+    pub fn num_items(&self) -> u64 {
+        return self.n;
+    }
+    pub fn len(&self) -> u64 {
+        return self.m;
+    }
+    pub fn expansion_rate(&self) -> f64 {
+        return self.epsilon;
+    }
+
+    pub fn encode(&mut self, list: &HashMap<u64, Scalar>) {
+        let mut pivots: Vec<usize> = Vec::with_capacity(self.n as usize);
+        let pos_band_range: u64 = self.m - self.kappa;
+        for (&key, &value) in list.iter() {
+            let seed = hash64(&(key ^ HASH_SEED));
+            let hash_pos = (seed % pos_band_range) as usize;
+            let hash_band: Vec<Scalar> = (0..self.kappa)
+                .map(|i| Scalar::from((seed >> i) & 0x01))
+                .collect();
+
+            self._matrix.push((hash_pos, (hash_band, value)));
+        }
+        // sort by position
+        self._matrix.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+        for row in 0..self.n as usize {
+            for i in 0..self.kappa as usize {
+                if Scalar::ZERO == self._matrix[row].1 .0[i] {
+                    continue;
+                }
+                let piv = self._matrix[row].0 + i;
+                pivots.push(piv);
+                // if the pivot is not one, divide the row to normalize it
+                if self._matrix[row].1 .0[i] != Scalar::ONE {
+                    let inv = self._matrix[row].1 .0[i].invert();
+                    self._matrix[row].1 .0[i] = Scalar::ONE;
+                    for j in i + 1..self.kappa as usize {
+                        self._matrix[row].1 .0[j] *= inv;
+                    }
+                    self._matrix[row].1 .1 *= inv;
+                }
+                // update the band from the following rows
+                for j in row + 1..self.n as usize {
+                    let (pos, (band, value)) = &self._matrix[j];
+                    // skip if the position is out of range
+                    if *pos > piv {
+                        continue;
+                    }
+                    // if found the non-zero position, subtract the band
+                    let under_pivot = piv - *pos;
+                    let multplier = band[under_pivot];
+                    let mut tem_scalar;
+                    if Scalar::ZERO != multplier {
+                        if Scalar::ONE != multplier {
+                            for k in i + 1..self.kappa as usize {
+                                tem_scalar = self._matrix[row].1 .0[k] * multplier;
+                                self._matrix[j].1 .0[under_pivot + k - i] -= tem_scalar;
+                            }
+                            tem_scalar = self._matrix[row].1 .1 * multplier;
+                            self._matrix[j].1 .1 -= tem_scalar;
+                        } else {
+                            for k in i + 1..self.kappa as usize {
+                                tem_scalar = self._matrix[row].1 .0[k] * multplier;
+                                self._matrix[j].1 .0[under_pivot + k - i] -= tem_scalar;
+                            }
+                            tem_scalar = self._matrix[row].1 .1;
+                            self._matrix[j].1 .1 -= tem_scalar;
+                        }
+                    }
+                    self._matrix[j].1 .0[under_pivot] = Scalar::ZERO;
+                }
+                break;
+            }
+        }
+        // band should not be all-zero
+        assert_eq!(pivots.len(), self.n as usize);
+        // back substitution
+        for row in (0..self.n as usize).rev() {
+            let (pos, (band, value)) = &self._matrix[row];
+            let piv = pivots[row];
+            let mut val = *value;
+            for i in 0..self.kappa as usize {
+                if Scalar::ZERO == band[i] {
+                    continue;
+                }
+                if (i + *pos) == piv {
+                    continue;
+                }
+                val -= self._data[i + *pos] * band[i];
+            }
+            self._data[piv] = val;
+        }
+        // finalize
+        for i in 0..self.m as usize {
+            self.data.push(&self._data[i] * RISTRETTO_BASEPOINT_TABLE);
+        }
+        self._matrix.clear();
+    }
+
+    pub fn decode(&self, key: u64) -> Point {
+        let pos_band_range: u64 = self.m - self.kappa;
+        let seed: u64 = hash64(&(key ^ HASH_SEED));
+        let pos: usize = (seed % pos_band_range) as usize;
+        let band: Vec<u64> = (0..self.kappa).map(|i| (seed >> i) & 0x01).collect();
+
+        let mut result = Point::identity();
+
+        for i in pos..self.kappa as usize + pos {
+            if 0 == band[i - pos] {
+                continue;
+            }
+            result += self.data[i];
+        }
+        return result;
+    }
 }
